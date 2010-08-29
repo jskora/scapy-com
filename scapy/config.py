a7f3a0a7a4eb3392103273754247114d691ab32c
Jochen Bartl <jochen.bartl@gmail.com>
2010-08-29 02:57:04 +0200

    Added OSPFv2 / OSPFv3 support
    Closes ticket #163
    
    TODO:
    
    - OspfIP6Field should be replaced as soon as IP6Field supports
      length_from / adjust functionality
    - OSPFv2 cryptographic authentication

--- scapy/config.py
+++ scapy/config.py
@@ -371,7 +371,7 @@ extensions_paths: path or list of paths where extensions are to be looked for
     temp_files = []
     netcache = NetCache()
     load_layers = ["l2", "inet", "dhcp", "dns", "dot11", "gprs", "hsrp", "inet6", "ir", "isakmp", "l2tp",
-                   "mgcp", "mobileip", "netbios", "netflow", "ntp", "ppp", "radius", "rip", "rtp",
+                   "mgcp", "mobileip", "netbios", "netflow", "ntp", "ospf", "ppp", "radius", "rip", "rtp",
                    "sebek", "skinny", "smb", "snmp", "tftp", "x509", "bluetooth", "dhcp6", "llmnr", "sctp", "vrrp" ]
     
 

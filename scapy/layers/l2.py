64802eb2f2a39f74789e7a4dd53617ac336c89ad
Sean Van Gorder <sean.van.gorder@gmail.com>
2010-09-13 15:46:13 -0400

    Better default value for Ether.type
    Ethernet type can't be 0, that makes it a Dot3 layer.

--- scapy/layers/l2.py
+++ scapy/layers/l2.py
@@ -146,7 +146,7 @@ class Ether(Packet):
     name = "Ethernet"
     fields_desc = [ DestMACField("dst"),
                     SourceMACField("src"),
-                    XShortEnumField("type", 0x0000, ETHER_TYPES) ]
+                    XShortEnumField("type", 0x0600, ETHER_TYPES) ]
     def hashret(self):
         return struct.pack("H",self.type)+self.payload.hashret()
     def answers(self, other):

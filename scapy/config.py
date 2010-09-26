4c580df65bf91359eea8daf67a0e3e29fb440fc2
Jochen Bartl <jochen.bartl@gmail.com>
2010-09-26 18:42:04 +0200

    Added EIGRP (Enhanced Interior Gateway Routing Protocol) support
    Closes ticket #117

--- scapy/config.py
+++ scapy/config.py
@@ -370,8 +370,8 @@ extensions_paths: path or list of paths where extensions are to be looked for
     stats_dot11_protocols = []
     temp_files = []
     netcache = NetCache()
-    load_layers = ["l2", "inet", "cdp", "dhcp", "dns", "dot11", "gprs", "hsrp", "inet6", "ir", "isakmp", "l2tp",
-                   "mgcp", "mobileip", "netbios", "netflow", "ntp", "ospf", "ppp", "radius", "rip", "rtp",
+    load_layers = ["l2", "inet", "cdp", "dhcp", "dns", "dot11", "eigrp", "gprs", "hsrp", "inet6", "ir", "isakmp",
+                   "l2tp", "mgcp", "mobileip", "netbios", "netflow", "ntp", "ospf", "ppp", "radius", "rip", "rtp",
                    "sebek", "skinny", "smb", "snmp", "tftp", "x509", "bluetooth", "dhcp6", "llmnr", "sctp", "vrrp" ]
     
 

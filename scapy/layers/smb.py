44cccfa77f6a1826a5aacc58e86d00135e0599e9
Sean Van Gorder <sean.van.gorder@gmail.com>
2010-11-09 12:06:56 -0500

    Added support for SECURITY_DESCRIPTOR data (SIDs, ACLs, ACEs)

--- scapy/layers/smb.py
+++ scapy/layers/smb.py
@@ -7,7 +7,7 @@
 SMB (Server Message Block), also known as CIFS.
 """
 
-import datetime
+import datetime,operator
 from scapy.packet import *
 from scapy.fields import *
 from scapy.layers.netbios import NBTDatagram,NBTSession
@@ -1077,6 +1077,227 @@ class SMB_FEA(Packet):
 
 ########################### Data Structures [MS-DTYP] ##########################
 
+class SIDField(StrField):
+    def __init__(self, name, default):
+        StrField.__init__(self, name, default)
+    def i2m(self, pkt, i):
+        if not i:
+            return ""
+        x = i.split("-")
+        if len(x) < 4 or x[0] != "S":
+            raise #XXX: not sure about this
+        m = ""
+        m += struct.pack("B", int(x[1]))
+        m += struct.pack("B", len(x)-3)
+        m += "\x00"*5 + struct.pack("B", int(x[2])) #XXX: verify this?
+        for a in x[3:]:
+            m += struct.pack("<I", int(a))
+        return m
+    def m2i(self, pkt, m):
+        x = ["S"]
+        x.append(str(ord(m[0])))
+        x.append(str(ord(m[7])))
+        m = m[8:]
+        while m:
+            x.append(str(struct.unpack("<I", m[0:4])[0]))
+            m = m[4:]
+        i = "-".join(x)
+        return i
+    def getfield(self, pkt, s):
+        if len(s) < 12:
+            return s,""
+        r = (ord(s[1])*4)+8
+        i = self.m2i(pkt, s[:r])
+        return s[r:],i
+
+class SID(Packet):
+    name = "Security Identifier (SID) [MS-DTYP]"
+    fields_desc = [SIDField("SID","S-1-0-0")]
+
+ace_types = {0x00: "ACCESS_ALLOWED",
+             0x01: "ACCESS_DENIED",
+             0x02: "SYSTEM_AUDIT",
+             0x03: "SYSTEM_ALARM",                   # reserved
+             0x04: "ACCESS_ALLOWED_COMPOUND",        # reserved
+             0x05: "ACCESS_ALLOWED_OBJECT",
+             0x06: "ACCESS_DENIED_OBJECT",
+             0x07: "SYSTEM_AUDIT_OBJECT",
+             0x08: "SYSTEM_ALARM_OBJECT",            # reserved
+             0x09: "ACCESS_ALLOWED_CALLBACK",
+             0x0A: "ACCESS_DENIED_CALLBACK",
+             0x0B: "ACCESS_ALLOWED_CALLBACK_OBJECT",
+             0x0C: "ACCESS_DENIED_CALLBACK_OBJECT",
+             0x0D: "SYSTEM_AUDIT_CALLBACK",
+             0x0E: "SYSTEM_ALARM_CALLBACK",          # reserved
+             0x0F: "SYSTEM_AUDIT_CALLBACK_OBJECT",
+             0x10: "SYSTEM_ALARM_CALLBACK_OBJECT",   # reserved
+             0x11: "SYSTEM_MANDATORY_LABEL" }
+
+ace_flags_object_Mask = ["RIGHT_DS_CREATE_CHILD","RIGHT_DS_DELETE_CHILD","res2","RIGHT_DS_SELF",
+                         "RIGHT_DS_READ_PROP","RIGHT_DS_WRITE_PROP","res6","res7",
+                         "RIGHT_DS_CONTROL_ACCESS"]+\
+                        _reserved_flags[9:16]+_ACCESS_MASK_common
+
+ace_flags_object_Flags = ["OBJECT_TYPE_PRESENT","INHERITED_OBJECT_TYPE_PRESENT"]+\
+                         _reserved_flags[2:32]
+
+ace_flags_sml_Mask = ["NO_WRITE_UP","NO_READ_UP","NO_EXECUTE_UP"]+\
+                     _reserved_flags[3:16]+_ACCESS_MASK_common
+
+class _ACE_HDR(Packet):
+    name = "Abstract ACE Header"
+    fields_desc = [ByteEnumField("AceType",0xFF,ace_types),
+                   LEFlagsField("AceFlags",0,8,["OI","CI","NP","IO",
+                                                "ID","res5","SA","FA"]),
+                   LEShortField("AceSize",None)]
+
+class ACE(Packet):
+    name = "Access Control Entry (ACE) [MS-DTYP]"
+    fields_desc = [_ACE_HDR,
+                   LEFlagsField("Mask",0,32,ACCESS_MASK),
+                   SIDField("SID","S-1-0-0")]
+    def post_build(self, p, pay):
+        p += pay
+        if self.AceSize is None:
+            l = len(p)
+            p = p[:2]+chr(l&0xff)+chr((l>>8)&0xff)+p[4:]
+        return p
+    def extract_padding(self, p):
+        return "",p
+    registered_ace = {}
+    @classmethod
+    def register_variant(cls):
+        cls.registered_ace[cls.AceType.default] = cls
+    @classmethod
+    def dispatch_hook(cls, pkt=None, *args, **kargs):
+        if pkt:
+            acetype = ord(pkt[0])
+            if acetype in cls.registered_ace:
+                return cls.registered_ace[acetype]
+        return cls
+
+class ACE_ACCESS_ALLOWED(ACE):
+    name = "ACE - ACCESS_ALLOWED_ACE [MS-DTYP]"
+    AceType = 0x00
+
+class ACE_ACCESS_DENIED(ACE):
+    name = "ACE - ACCESS_DENIED_ACE [MS-DTYP]"
+    AceType = 0x01
+
+class ACE_SYSTEM_AUDIT(ACE):
+    name = "ACE - SYSTEM_AUDIT_ACE [MS-DTYP]"
+    AceType = 0x02
+
+class ACE_ACCESS_ALLOWED_OBJECT(ACE):
+    name = "ACE - ACCESS_ALLOWED_OBJECT_ACE [MS-DTYP]"
+    AceType = 0x05
+    fields_desc = [_ACE_HDR,
+                   LEFlagsField("Mask",0,32,ace_flags_object_Mask),
+                   LEFlagsField("Flags",0,32,ace_flags_object_Flags),
+                   StrFixedLenField("ObjectType","",16), #TODO: GUID type
+                   StrFixedLenField("InheritedObjectType","",16), #TODO: GUID type
+                   SIDField("SID","S-1-0-0")]
+
+class ACE_ACCESS_DENIED_OBJECT(ACE_ACCESS_ALLOWED_OBJECT):
+    name = "ACE - ACCESS_DENIED_OBJECT_ACE [MS-DTYP]"
+    AceType = 0x06
+
+class ACE_SYSTEM_AUDIT_OBJECT(ACE_ACCESS_ALLOWED_OBJECT):
+    name = "ACE - SYSTEM_AUDIT_OBJECT_ACE [MS-DTYP]"
+    AceType = 0x07
+
+class ACE_ACCESS_ALLOWED_CALLBACK(ACE):
+    name = "ACE - ACCESS_ALLOWED_CALLBACK_ACE [MS-DTYP]"
+    AceType = 0x09
+    fields_desc = [ACE,
+                   StrLenField("ApplicationData","",
+                               length_from=lambda pkt:pkt.AceSize-8-pkt.getfieldlen("SID"))]
+
+class ACE_ACCESS_DENIED_CALLBACK(ACE_ACCESS_ALLOWED_CALLBACK):
+    name = "ACE - ACCESS_DENIED_CALLBACK_ACE [MS-DTYP]"
+    AceType = 0x0A
+
+class ACE_ACCESS_ALLOWED_CALLBACK_OBJECT(ACE):
+    name = "ACE - ACCESS_ALLOWED_CALLBACK_OBJECT_ACE [MS-DTYP]"
+    AceType = 0x0B
+    fields_desc = [ACE_ACCESS_ALLOWED_OBJECT,
+                   StrLenField("ApplicationData","",
+                               length_from=lambda pkt:pkt.AceSize-8-pkt.getfieldlen("SID"))]
+
+class ACE_ACCESS_DENIED_CALLBACK_OBJECT(ACE_ACCESS_ALLOWED_CALLBACK_OBJECT):
+    name = "ACE - ACCESS_DENIED_CALLBACK_OBJECT_ACE [MS-DTYP]"
+    AceType = 0x0C
+
+class ACE_SYSTEM_AUDIT_CALLBACK(ACE_ACCESS_ALLOWED_CALLBACK):
+    name = "ACE - SYSTEM_AUDIT_CALLBACK_ACE [MS-DTYP]"
+    AceType = 0x0D
+
+class ACE_SYSTEM_AUDIT_CALLBACK_OBJECT(ACE_ACCESS_ALLOWED_CALLBACK_OBJECT):
+    name = "ACE - SYSTEM_AUDIT_CALLBACK_OBJECT_ACE [MS-DTYP]"
+    AceType = 0x0F
+
+class ACE_SYSTEM_MANDATORY_LABEL(ACE):
+    name = "ACE - SYSTEM_MANDATORY_LABEL_ACE [MS-DTYP]"
+    AceType = 0x11
+    fields_desc = [_ACE_HDR,
+                   LEFlagsField("Mask",0,32,ace_flags_sml_Mask),
+                   SIDField("SID","S-1-0-0")]
+
+class ACL(Packet):
+    name = "Access Control List (ACL) [MS-DTYP]"
+    fields_desc = [ByteEnumField("AclRevision",2,{2:"ACL_REVISION",4:"ACL_REVISION_DS"}),
+                   ByteField("Sbz1",0),
+                   FieldLenField("AclSize",None,length_of="ACEs",fmt="<H",
+                                 adjust=lambda pkt,x:x+8),
+                   FieldLenField("AceCount",None,count_of="ACEs",fmt="<H"),
+                   LEShortField("Sbz2",0),
+                   PacketListField("ACEs",[],ACE,count_from=lambda pkt:pkt.AceCount)]
+
+class SACL(ACL):
+    name = "System Access Control List (SACL) [MS-DTYP]"
+
+class DACL(ACL):
+    name = "Discretionary Access Control List (DACL) [MS-DTYP]"
+
+class SecurityDescriptorDataField(PacketListField):
+    def __init__(self, name, default):
+        PacketListField.__init__(self, name, default, Raw)
+    def _get_chunk_list(self, pkt):
+        chunks = [{"off":pkt.OffsetOwner, "cls":SID},
+                  {"off":pkt.OffsetGroup, "cls":SID},
+                  {"off":pkt.OffsetSacl,  "cls":SACL},
+                  {"off":pkt.OffsetDacl,  "cls":DACL}]
+        chunks = [c for c in chunks if c["off"] != 0]
+        if all(c["off"] != 20 for c in chunks):
+            chunks.append({"off":20, "cls":Raw})
+        return sorted(chunks, key=operator.itemgetter("off"))
+    def getfield(self, pkt, s):
+        chunks = self._get_chunk_list(pkt)
+        lst = []
+        for i in range(len(chunks)):
+            cls = chunks[i]["cls"]
+            off = chunks[i]["off"]-20
+            if len(chunks) > i+1:
+                next = chunks[i+1]["off"]-20
+            else:
+                next = len(s)
+            try:
+                p = cls(s[off:next])
+            except Exception:
+                if conf.debug_dissector:
+                    raise
+                p = conf.raw_layer(load=s[off:])
+            lst.append(p)
+        return "",lst
+    def addfield(self, pkt, s, val):
+        chunks = self._get_chunk_list(pkt)
+        for i in range(len(chunks)):
+            off = chunks[i]["off"]
+            if len(s) < off:
+                s += "\x00"*(off-len(s))
+            s = s[:off]+str(val[i])
+        return s
+
 class SECURITY_DESCRIPTOR(Packet):
     name = "SECURITY_DESCRIPTOR [MS-DTYP]"
     fields_desc = [ByteField("Revision",1),
@@ -1089,7 +1310,7 @@ class SECURITY_DESCRIPTOR(Packet):
                    LEIntField("OffsetGroup",0),
                    LEIntField("OffsetSacl",0),
                    LEIntField("OffsetDacl",0),
-                   StrField("Data","")] #XXX: data fields not implemented
+                   SecurityDescriptorDataField("Data",[])]
 
 
 ########################### Data Structures [MS-FSCC] ##########################

8e19b20eb9c46ec99535f027a06c1c9f3c73e680
Adam P <ziherung.pl@gmail.com>
2012-05-26 16:56:44 +0200

    Add some RadioTap fields

--- scapy/layers/dot11.py
+++ scapy/layers/dot11.py
@@ -113,6 +113,142 @@ class PrismHeader(Packet):
             return self.payload.answers(other.payload)
         else:
             return self.payload.answers(other)
+        
+class RadioTapField(Field):
+    """Field implementing addfield,getfield shared by other RadioTap Fields"""
+    def addfield(self, pkt, s, val):
+        if self.is_applicable(pkt):
+            return Field.addfield(self, pkt, s, val)
+        else:
+            return s
+    
+    def getfield(self, pkt, s):
+        if self.is_applicable(pkt):
+            return Field.getfield(self, pkt, s)
+        else:
+            return s,None
+
+class RadioTapByteField(ByteField):
+    """One byte field, share functionality across other one byte RadioTapFields""" 
+    def addfield(self, pkt, s, val):
+        if self.is_applicable(pkt):
+            return ByteField.addfield(self, pkt, s, val)
+        else:
+            return s
+    
+    def getfield(self, pkt, s):
+        if self.is_applicable(pkt):
+            return ByteField.getfield(self, pkt, s)
+        else:
+            return s,None
+
+
+class RadioTapTSFTField(RadioTapField):
+    def __init__(self,name,default):
+        Field.__init__(self,name,default,"8s")
+    def is_applicable(self, pkt):
+        '''Returns True if Field is present according to RadioTap.present field,
+        existence of pkt.present must be checked - otherwise won't read pcaps properly'''
+        # first part of condition required by RadioTap.get_decoded_length
+        if (pkt.present != None) and (pkt.present & 0x1 == 0x1):
+            return 1
+        return 0
+    
+    def i2h(self,pkt,x):
+        if (x == None) or (pkt.present == None):
+            return 0
+        return struct.unpack('Q',x)[0]
+    
+    
+class RadioTapFlagsField(RadioTapByteField):
+    def is_applicable(self, pkt):
+        if (pkt.present != None) and ( pkt.present & 0x2 == 0x2 ): 
+            return 1
+        return 0
+
+
+class RadioTapRateField(RadioTapByteField):
+    def is_applicable(self, pkt):
+        if (pkt.present != None) and ( pkt.present & 0x4 == 0x4 ): 
+            return 1
+        return 0
+        
+    def i2h(self,pkt,x):
+        if ( x == None ) or ( pkt.present == None ):
+            return 0
+        return "%s Mbps" % (0.5*x) # RadioTap Rate unit is 500 Kbps
+        
+
+class RadioTapChannelField(RadioTapField):
+    def __init__(self,name,default):
+        Field.__init__(self,name,default,"4s")
+        
+    def is_applicable(self, pkt):
+        if ( pkt.present != None ) and ( pkt.present & 0x8 == 0x8 ): 
+                return 1
+        return 0
+    
+    def i2h(self,pkt,x):
+        if ( x == None ) or ( pkt.present == None ):
+            return 0
+        return int("%02x%02x" %  (ord(x[1]),ord(x[0])), 16)
+
+
+class RadioTapFHSSField(RadioTapField):
+    def __init__(self,name,default):
+        Field.__init__(self,name,default,"2s")
+    def is_applicable(self, pkt):
+        if ( pkt.present != None ) and ( pkt.present & 0x10 == 0x10 ): 
+            return 1
+        return 0
+
+
+class RadioTapAntennaSignalField(SignedByteField):
+    def is_applicable(self, pkt):
+        if ( pkt.present != None ) and ( pkt.present & 0x20 == 0x20 ):
+            return 1
+        return 0
+    
+    def addfield(self, pkt, s, val):
+        if self.is_applicable(pkt):
+            return SignedByteField.addfield(self, pkt, s, val)
+        else:
+            return s
+    
+    def getfield(self, pkt, s):
+        if self.is_applicable(pkt):
+            return SignedByteField.getfield(self, pkt, s)
+        else:
+            return s,None
+
+
+class RadioTapAntennaNoiseField(SignedByteField):
+    def is_applicable(self, pkt):
+        if ( pkt.present != None ) and ( pkt.present & 0x40 == 0x40 ): 
+            return 1
+        return 0
+    
+    def addfield(self, pkt, s, val):
+        if self.is_applicable(pkt):
+            return SignedByteField.addfield(self, pkt, s, val)
+        else:
+            return s
+    
+    def getfield(self, pkt, s):
+        if self.is_applicable(pkt):
+            return SignedByteField.getfield(self, pkt, s)
+        else:
+            return s,None
+
+    
+class RadioTapLockQualityField(RadioTapField):
+    def __init__(self,name,default):
+        Field.__init__(self,name,default,"2s")
+    def is_applicable(self, pkt):
+        if ( pkt.present != None ) and ( pkt.present & 0x80 == 0x80 ): 
+            return 1
+        return 0
+
 
 class RadioTap(Packet):
     name = "RadioTap dummy"
@@ -123,11 +259,41 @@ class RadioTap(Packet):
                                                       'FHSS','dBm_AntSignal','dBm_AntNoise','Lock_Quality',
                                                       'TX_Attenuation','dB_TX_Attenuation','dBm_TX_Power','Antenna',
                                                       'dB_AntSignal','dB_AntNoise','RX_Flags', '',
-                                                      '','','','MCS',
+                                                      '','','XChannel','MCS',
                                                       '','','','',
                                                       '','','','',
                                                       '','Radiotap_Namespace','Vendor_Namespace','Ext']),
-                    StrLenField('notdecoded', "", length_from= lambda pkt:pkt.len-8) ]
+                    RadioTapTSFTField('TSFT',0),
+                    RadioTapFlagsField('Flags',0),
+                    RadioTapRateField('Rate',0),
+                    RadioTapChannelField('Channel',0),
+                    RadioTapFHSSField('FHSS',0),
+                    RadioTapAntennaSignalField('dBm_AntSignal',0),
+                    RadioTapAntennaNoiseField('dBm_AntNoise',0),
+                    RadioTapLockQualityField('Lock_Quality',0),
+                    StrLenField('notdecoded', "", length_from= lambda pkt:pkt.len-pkt.get_decoded_length()) ]
+                    #StrLenField('notdecoded', "", length_from= lambda pkt:pkt.len-8) ]
+                    
+    def get_decoded_length(self):
+        res = 8 # set in previous version of StrLenField
+        if self.fields_desc[4].is_applicable(self): # TSFT
+            res+=8
+        if self.fields_desc[5].is_applicable(self):
+            res+=1
+        if self.fields_desc[6].is_applicable(self):
+            res+=1
+        if self.fields_desc[7].is_applicable(self):
+            res+=4
+        if self.fields_desc[8].is_applicable(self):
+            res+=2
+        if self.fields_desc[9].is_applicable(self):
+            res+=1
+        if self.fields_desc[10].is_applicable(self):
+            res+=1
+        if self.fields_desc[11].is_applicable(self):
+            res+=2
+        return res
+            
 
 class PPI(Packet):
     name = "Per-Packet Information header (partial)"

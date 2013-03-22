82d91cd19b82b66dbb4160ca7b1debb3bd8cafb7
Philippe DePass <depassp@gmail.com>
2013-03-22 00:17:46 -0400

    Present the fields of a BGP Path Attribute more usefully
    Also handle extended length properly

--- scapy/contrib/bgp.py
+++ scapy/contrib/bgp.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python 
+#! /usr/bin/env python
 
 # http://trac.secdev.org/scapy/ticket/162
 
@@ -8,7 +8,6 @@
 from scapy.packet import *
 from scapy.fields import *
 from scapy.layers.inet import TCP
-
 
 class BGPIPField(Field):
     """Represents how bgp dose an ip prefix in (length, prefix)"""
@@ -33,7 +32,7 @@ class BGPIPField(Field):
         """internal (ip as bytes, mask as int) to machine"""
         mask, ip = i
         ip = inet_aton( ip )
-        return struct.pack(">B",mask) + ip[:self.mask2iplen(mask)] 
+        return struct.pack(">B",mask) + ip[:self.mask2iplen(mask)]
     def addfield(self, pkt, s, val):
         return s+self.i2m(pkt, val)
     def getfield(self, pkt, s):
@@ -48,13 +47,14 @@ class BGPHeader(Packet):
     """The first part of any BGP packet"""
     name = "BGP header"
     fields_desc = [
-    XBitField("marker",0xffffffffffffffffffffffffffffffff, 0x80 ),
-    ShortField("len", None),
-    ByteEnumField("type", 4, {0:"none", 1:"open",2:"update",3:"notification",4:"keep_alive"}),
-    ]
+        XBitField("marker", 0xffffffffffffffffffffffffffffffff, 0x80 ),
+        ShortField("len", None),
+        ByteEnumField("type", 4, {0:"none", 1:"open",2:"update",3:"notification",4:"keep_alive"}),
+        ]
     def post_build(self, p, pay):
         if self.len is None and pay:
             l = len(p) + len(pay)
+            assert(19 < l < 4096)
             p = p[:16]+struct.pack("!H", l)+p[18:]
         return p+pay
 
@@ -62,10 +62,10 @@ class BGPOptionalParameter(Packet):
     """Format of optional Parameter for BGP Open"""
     name = "BGP Optional Parameters"
     fields_desc = [
-    ByteField("type", 2),
-    ByteField("len", None),
-    StrLenField("value", "",  length_from = lambda x: x.len),
-    ]
+        ByteField("type", 2),
+        ByteField("len", None),
+        StrLenField("value", "",  length_from = lambda x: x.len),
+        ]
     def post_build(self,p,pay):
         if self.len is None:
             l = len(p) - 2 # 2 is length without value
@@ -77,15 +77,16 @@ class BGPOptionalParameter(Packet):
 
 class BGPOpen(Packet):
     """ Opens a new BGP session"""
-    name = "BGP Open Header"
+    name = "BGP Open"
     fields_desc = [
-    ByteField("version", 4),
-    ShortField("AS", 0),
-    ShortField("hold_time", 0),
-    IPField("bgp_id","0.0.0.0"),
-    ByteField("opt_parm_len", None),
-    PacketListField("opt_parm",[], BGPOptionalParameter, length_from=lambda p:p.opt_parm_len),
-    ]
+        ByteField("version", 4),
+        ShortField("AS", 0),
+        ShortField("hold_time", 0),
+        IPField("bgp_id","0.0.0.0"),
+        ByteField("opt_parm_len", None),
+        PacketListField("opt_parm",[], BGPOptionalParameter,
+            length_from=lambda p:p.opt_parm_len),
+        ]
     def post_build(self, p, pay):
         if self.opt_parm_len is None:
             l = len(p) - 10 # 10 is regular length with no additional options
@@ -95,39 +96,114 @@ class BGPOpen(Packet):
 class BGPAuthenticationData(Packet):
     name = "BGP Authentication Data"
     fields_desc = [
-    ByteField("AuthenticationCode", 0),
-    ByteField("FormMeaning", 0),
-    FieldLenField("Algorithm", 0),
-    ]
+        ByteField("AuthenticationCode", 0),
+        ByteField("FormMeaning", 0),
+        FieldLenField("Algorithm", 0),
+        ]
+
+class ASPathSegment(Packet):
+    name = "AS_PATH Segment"
+    fields_desc = [
+        ByteEnumField("ps_type", 2, {1:"AS_SET", 2:"AS_SEQUENCE"}),
+        FieldLenField("ps_count", None, fmt="!B", count_of="ps_value"),
+        FieldListField("ps_value", [], ShortField("", 0),
+            count_from=lambda pkt: pkt.ps_count),
+        ]
+
+    def post_build(self, p, pay):
+        if self.ps_value and self.ps_count is None:
+            l = len(self.ps_value)
+            p = p[:1] + struct.pack("!B",l) + p[2:]
+        return p+pay
+
+    def extract_padding(self, p):
+        return "",p
+
+class Aggregator(Packet):
+    name = "AGGREGATOR Attribute"
+    fields_desc = [
+        ShortField("last_as_number", 0),
+        IPField("address", "0.0.0.0"),
+        ]
 
 class BGPPathAttribute(Packet):
     "the attribute of total path"
-    name = "BGP Attribute fields"
+    name = "BGP Path Attribute"
     fields_desc = [
-    FlagsField("flags", 0x40, 8, ["NA0","NA1","NA2","NA3","Extended-Length","Partial","Transitive","Optional"]), #Extened leght may not work
-    ByteEnumField("type", 1, {1:"ORIGIN", 2:"AS_PATH", 3:"NEXT_HOP", 4:"MULTI_EXIT_DISC", 5:"LOCAL_PREF", 6:"ATOMIC_AGGREGATE", 7:"AGGREGATOR"}),
-    ByteField("attr_len", None),
-    StrLenField("value", "", length_from = lambda p: p.attr_len),
+    FlagsField("flags", 0x40, 8, ["NA0","NA1","NA2","NA3",
+        "Extended-Length","Partial","Transitive","Optional"]),
+    ByteEnumField("type", None, {1:"ORIGIN", 2:"AS_PATH", 3:"NEXT_HOP",
+        4:"MULTI_EXIT_DISC", 5:"LOCAL_PREF", 6:"ATOMIC_AGGREGATE",
+        7:"AGGREGATOR"}),
+    ConditionalField(
+        ByteField("len", None),
+        cond=lambda pkt: (pkt.flags>>4)&1 == 0),
+    ConditionalField(
+        ShortField("extlen", None),
+        cond=lambda pkt: (pkt.flags>>4)&1 == 1),
+
+    # ORIGIN
+    ConditionalField(
+        ByteEnumField("origin", 0, {0:"IGP", 1:"EGP", 2:"INCOMPLETE"}),
+        cond=lambda pkt:pkt.type==1),
+    # AS_PATH
+    ConditionalField(
+        PacketField("as_path", ASPathSegment(), ASPathSegment),
+        cond=lambda pkt:pkt.type==2),
+    # NEXT_HOP
+    ConditionalField(
+        IPField("next_hop", "0.0.0.0"),
+        cond=lambda pkt:pkt.type==3),
+    # MULTI_EXIT_DISC
+    ConditionalField(
+        IntField("multi_exit_disc", 0),
+        cond=lambda pkt:pkt.type==4),
+    # LOCAL_PREF
+    ConditionalField(
+        IntField("local_pref", 0),
+        cond=lambda pkt:pkt.type==5),
+    # ATOMIC_AGGREGATE is of length 0 (no value)
+    # AGGREGATOR
+    ConditionalField(
+        PacketField("aggregator", Aggregator(), Aggregator),
+        cond=lambda pkt:pkt.type==7),
     ]
+
     def post_build(self, p, pay):
-        if self.attr_len is None:
-            l = len(p) - 3 # 3 is regular length with no additional options
-            p = p[:2] + struct.pack("!B",l)  +p[3:]
+        if self.len is None and self.extlen is None:
+            flags = ord(p[0:1])
+            l = len(p) + len(pay)
+            if l - 4 > 0xFFFF:
+                raise Error("Packet is too large")
+            elif l - 4 > 0xFF:
+                l -= 4
+                flags |= 1<<4
+                p = chr(flags) + p[1:2] + struct.pack("!H", l) + p[4:]
+            else:
+                l -= 3
+                flags &= ~(1<<4)
+                p = chr(flags) + p[1:2] + struct.pack("!B", l)  +p[3:]
         return p+pay
+
     def extract_padding(self, p):
         """any thing after this packet is extracted is padding"""
         return "",p
 
 class BGPUpdate(Packet):
     """Update the routes WithdrawnRoutes = UnfeasiableRoutes"""
-    name = "BGP Update fields"
+    name = "BGP Update"
     fields_desc = [
-    ShortField("withdrawn_len", None),
-    FieldListField("withdrawn",[], BGPIPField("","0.0.0.0/0"), length_from=lambda p:p.withdrawn_len),
-    ShortField("tp_len", None),
-    PacketListField("total_path", [], BGPPathAttribute, length_from = lambda p: p.tp_len),
-    FieldListField("nlri",[], BGPIPField("","0.0.0.0/0"), length_from=lambda p:p.underlayer.len - 23 - p.tp_len - p.withdrawn_len), # len should be BGPHeader.len
-    ]
+        FieldLenField("withdrawn_len", 0,
+            length_of=lambda p: p.withdrawn),
+        FieldListField("withdrawn", [], BGPIPField("","0.0.0.0/0"),
+            length_from=lambda p:p.withdrawn_len),
+        FieldLenField("tp_len", 0,
+            length_of=lambda p: p.total_path),
+        PacketListField("total_path", [], BGPPathAttribute,
+            length_from=lambda p: p.tp_len),
+        FieldListField("nlri", [], BGPIPField("","0.0.0.0/0"),
+            length_from=lambda p:p.underlayer.len - 23 - p.tp_len - p.withdrawn_len), # len should be BGPHeader.len
+        ]
     def post_build(self,p,pay):
         wl = self.withdrawn_len
         subpacklen = lambda p: len ( str( p ))
@@ -143,27 +219,27 @@ class BGPUpdate(Packet):
 class BGPNotification(Packet):
     name = "BGP Notification fields"
     fields_desc = [
-    ByteEnumField("ErrorCode",0,{1:"Message Header Error",2:"OPEN Message Error",3:"UPDATE Messsage Error",4:"Hold Timer Expired",5:"Finite State Machine",6:"Cease"}),
-    ByteEnumField("ErrorSubCode",0,{1:"MessageHeader",2:"OPENMessage",3:"UPDATEMessage"}),
-    LongField("Data", 0),
-    ]
+        ByteEnumField("ErrorCode",0,{1:"Message Header Error",2:"OPEN Message Error",3:"UPDATE Messsage Error",4:"Hold Timer Expired",5:"Finite State Machine",6:"Cease"}),
+        ByteEnumField("ErrorSubCode",0,{1:"MessageHeader",2:"OPENMessage",3:"UPDATEMessage"}),
+        LongField("Data", 0),
+        ]
 
 class BGPErrorSubcodes(Packet):
     name = "BGP Error Subcodes"
     Fields_desc = [
-    ByteEnumField("MessageHeader",0,{1:"Connection Not Synchronized",2:"Bad Message Length",3:"Bad Messsage Type"}),
-    ByteEnumField("OPENMessage",0,{1:"Unsupported Version Number",2:"Bad Peer AS",3:"Bad BGP Identifier",4:"Unsupported Optional Parameter",5:"Authentication Failure",6:"Unacceptable Hold Time"}),
-    ByteEnumField("UPDATEMessage",0,{1:"Malformed Attribute List",2:"Unrecognized Well-Known Attribute",3:"Missing Well-Known Attribute",4:"Attribute Flags Error",5:"Attribute Length Error",6:"Invalid ORIGIN Attribute",7:"AS Routing Loop",8:"Invalid NEXT_HOP Attribute",9:"Optional Attribute Error",10:"Invalid Network Field",11:"Malformed AS_PATH"}),
-    ]
+        ByteEnumField("MessageHeader",0,{1:"Connection Not Synchronized",2:"Bad Message Length",3:"Bad Messsage Type"}),
+        ByteEnumField("OPENMessage",0,{1:"Unsupported Version Number",2:"Bad Peer AS",3:"Bad BGP Identifier",4:"Unsupported Optional Parameter",5:"Authentication Failure",6:"Unacceptable Hold Time"}),
+        ByteEnumField("UPDATEMessage",0,{1:"Malformed Attribute List",2:"Unrecognized Well-Known Attribute",3:"Missing Well-Known Attribute",4:"Attribute Flags Error",5:"Attribute Length Error",6:"Invalid ORIGIN Attribute",7:"AS Routing Loop",8:"Invalid NEXT_HOP Attribute",9:"Optional Attribute Error",10:"Invalid Network Field",11:"Malformed AS_PATH"}),
+        ]
 
 bind_layers( TCP,             BGPHeader,   dport=179)
 bind_layers( TCP,             BGPHeader,   sport=179)
 bind_layers( BGPHeader,       BGPOpen,     type=1)
 bind_layers( BGPHeader,       BGPUpdate,   type=2)
+bind_layers( BGPHeader,       BGPNotification,   type=3)
 bind_layers( BGPHeader,       BGPHeader,   type=4)
 
-
 if __name__ == "__main__":
-    from scapy.main import interact
+    from scapy.all import interact
     interact(mydict=globals(), mybanner="BGP addon .05")
 
